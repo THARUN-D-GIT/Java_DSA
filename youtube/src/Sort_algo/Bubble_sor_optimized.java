56b0b32cd9941d21ffd257191fe3173a69a803e4
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-05-17 00:37:56 +0530

    Added and updated DSA array programs

---
+++ youtube/src/Sort_algo/Bubble_sor_optimized.java
@@ -0,0 +1,71 @@
+package Sort_algo;
+
+import java.util.Scanner;
+
+public class Bubble_sor_optimized {
+    static void main(String[] args) {
+        /*
+        optimized version
+        after each pass largest element are sorted in order at the end
+        for that run the inner loop n-1-i
+
+         */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the length of the array");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("enter the array elements");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            //print(arr);
+        }
+        print(arr);
+        System.out.println();
+
+       /* for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+
+        */
+        int swap=0;
+        boolean flag = true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1;j++){
+                if(arr[j]>arr[j+1]){
+                    flag=false;
+                    break;
+                }
+
+            }
+            if(flag) break;
+
+            for(int j=0;j<n-1;j++){
+                if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                swap++;
+            }
+        }}
+        print(arr);
+        System.out.println();
+        System.out.println(swap);
+    }
+
+    static void print(int [] arr) {
+        for(int elem:arr){
+            System.out.print(elem+" ");
+        }
+
+    }
+
+
+}
