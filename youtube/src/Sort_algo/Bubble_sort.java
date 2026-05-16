976d45e3d77be4144606ead0cc00b75872ad14c2
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-05-17 00:02:48 +0530

    Added and updated DSA array programs

--- youtube/src/Sort_algo/Bubble_sort.java
+++ youtube/src/Sort_algo/Bubble_sort.java
@@ -8,18 +8,20 @@ public class Bubble_sort {
         System.out.println("enter the length of the array");
         int n = sc.nextInt();
         int [] arr = new int[n];
+        System.out.println("enter the array elements");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
             //print(arr);
         }
         print(arr);
+        System.out.println();
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i]>arr[j]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
             }
         }
