0e17a83813270f4be37879d3eb28ed711650b25a
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-05-16 23:36:20 +0530

    Added and updated DSA array programs

--- youtube/src/Array/check_array_sorted.java
+++ youtube/src/Array/check_array_sorted.java
@@ -2,15 +2,17 @@ package Array;
 
 public class check_array_sorted {
     static void main(String[] args) {
-        int [] arr={1,2,3,4,5,6};
-        boolean flag=false;
-        for(int i=0;i<arr.length;i++){
-            for(int  j=0;j<arr.length;j++){
-            if(arr[i]<=arr[j]){
-                flag=true;
+        int [] arr={1,2,3,4,7,5,6};
+        boolean flag=true;
+        for(int i=0;i<arr.length-1;i++){
+
+            if(arr[i]>arr[i+1]){
+                flag=false;
+
             }
-        }}
-        if(!flag){
+
+        }
+        if(flag==false){
             System.out.println("array is not sorted");
         }
         else {
