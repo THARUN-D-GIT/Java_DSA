edb647e1a720c2fd039038b425fa799a502ecb3f
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-05-17 13:30:02 +0530

    Added and updated DSA array programs

---
+++ youtube/src/Sort_algo/move_0s_toEnd.java
@@ -0,0 +1,52 @@
+package Sort_algo;
+
+import java.util.Scanner;
+
+public class move_0s_toEnd {
+    static void main(String[] args) {
+        /*
+        moving all zeros in an array to the end without changing the order of other elements
+        ex:
+        {1,-1,0,2,4,0,0,0,9,8,7,0}
+        o/p:
+        {1,-1,2,4,9,8,7,0,0,0,0,0}
+
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of the array");
+        int n = sc.nextInt();
+        int [] arr=new int[n];
+        System.out.println("enter the elements of the array");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        print(arr);
+        System.out.println();
+      for(int i=0;i<n;i++){
+            for(int j=0;j<n-1;j++){
+                if(arr[j]==0){
+                    int  temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+
+       /* for(int i=0;i<n-1;i++){
+            if(arr[i]==0){
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+
+        */
+        print(arr);
+    }
+    static void print(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
