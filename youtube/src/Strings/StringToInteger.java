7d7d00526e1ab23c9b899c696195c7317cb1e5eb
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-06-09 22:54:33 +0530

    Added and updated DSA array programs

---
+++ youtube/src/Strings/StringToInteger.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringToInteger {
+    static void main(String[] args) {
+        String s1="4536234654";
+        int n1=Integer.parseInt(s1);
+        System.out.println(n1+1);
+    }
+}
