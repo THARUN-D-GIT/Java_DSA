8818d83db4d34e2354b744f3ede0941e49aa9115
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-06-09 22:54:52 +0530

    Added and updated DSA array programs

---
+++ youtube/src/Strings/SubStringOfString.java
@@ -0,0 +1,27 @@
+package Strings;
+
+public class SubStringOfString {
+    static void main(String[] args) {
+        //we cannot change individual character to a string (Immutability)
+        String str="RamanandVerma";
+        //SubString:Continuous part of a String
+        /*
+        str.substring(i) ->prints all the character of a string from i to  end
+        str.substring(i,j) ->prints ALL THE CHARACTER FROM I TO J-1
+
+        PRINT ALL SUBSTRINGS:
+                    s=gopi
+              substring are -> g,go,gop,gopi,o,op,opi,p,pi,i
+         */
+        String str2="tarun";
+        //System.out.println(str.substring(0,8)+" "+(str.substring(8)));
+        //printing all the substrings
+        System.out.println("substring of {str} are");
+        for(int i=0;i<str2.length();i++){
+            for(int j=i;j<str2.length();j++){
+
+                System.out.println(str2.substring(i,j+1));
+            }
+        }
+    }
+}
