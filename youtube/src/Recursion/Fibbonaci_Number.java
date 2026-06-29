ee79025695d7bade68196c8e6cc652bc2d9276f9
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-06-29 23:23:36 +0530

    Added and updated DSA array programs

--- youtube/src/Recursion/Fibbonaci_Number.java
+++ youtube/src/Recursion/Fibbonaci_Number.java
@@ -8,9 +8,19 @@ public class Fibbonaci_Number {
         System.out.print("Enter a number : ");
         int n=sc.nextInt();
         //using object creation
-        Fibbonaci_Number f=new Fibbonaci_Number ();
+       // Fibbonaci_Number f=new Fibbonaci_Number ();
         //f.fib
-        System.out.println("fibonacci sequence sum untill:"+n+" is: "+f.fib(n));
+        //System.out.println("fibonacci sequence sum untill:"+n+" is: "+f.fib(n));
+
+        //using for loop
+         int a=0,b=1;
+         for(int i=0;i<n;i++){
+             System.out.print(a+" ");
+             int c=a+b;
+             a=b;
+             b=c;
+         }
+
 
     }
     public  int fib(int n) {
--- youtube/src/Recursion/GCD.java
+++ youtube/src/Recursion/GCD.java
@@ -14,14 +14,36 @@ public class GCD {
         int n1=sc.nextInt();
         int n2=sc.nextInt();
         int gcd=0;
-       for(int i=1;i<Math.min(n1,n2);i++)
-       {
-           if(n1%i==0 && n2%i==0)
-           {
-               gcd=i;
-           }
-       }
-        System.out.println(gcd);
+//       for(int i=1;i<Math.min(n1,n2);i++)
+//       {
+//           if(n1%i==0 && n2%i==0)
+//           {
+//               gcd=i;
+//           }
+//       }
+//        System.out.println(gcd);
+        /*
+        using long division method
+        first divide the larger number by small number
+        then the divisor becomes dividend for the next step
+        repeated until the remainder is 0
+        a->small number->divisor
+        b->larger number-> dividend
+        b%a-> ==0 exits and divisor is GCD
+        if not then
+        a->b%a;
+        b->a
+
+
+
+         */
+        //using recursion
+        System.out.println(gcd(n1,n2));
     }
+    public static int gcd(int a,int b){
+        if(a==0) return b;
+        return gcd(b%a,a);
+    }
+    //use of helper function to find minimum
 
 }
