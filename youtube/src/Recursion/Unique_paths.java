4d2e2ffabc206b335d6b814472d3dced4acc1c1b
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-06-30 22:26:39 +0530

    Added and updated DSA array programs

---
+++ youtube/src/Recursion/Unique_paths.java
@@ -0,0 +1,47 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Unique_paths {
+    static void main(String[] args) {
+        /*
+        There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+
+
+Constraints:
+
+1 <= m, n <= 100
+         */
+        //AGAIN TLE ERROR
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of m and n:");
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println("number of ways to reach the goal:"+" "+uniquePaths(m,n));
+       // System.out.println(uniquePaths(m,n));
+    }
+    public  static int uniquePaths(int m, int n) {
+        if(m==1 || n==1) return 1;
+        else return uniquePaths(m-1,n)+uniquePaths(m,n-1);
+    }
+}
