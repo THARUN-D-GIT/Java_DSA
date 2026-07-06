9ad7a1675bf78117d2c368a27911158f083661f6
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-07-06 22:27:40 +0530

    Added and updated DSA array programs

--- youtube/src/Recursion/PreInPost.java
+++ youtube/src/Recursion/PreInPost.java
@@ -2,10 +2,20 @@ package Recursion;
 
 public class PreInPost {
     static void main(String[] args) {
-        pip(2);
+        pip(3);
         /*
         return
         terminates the function that it has been called
+        for better understanding of the problem
+        refer class notes pdf concept
+        pre in post using Euler tour tree
+
+         */
+        /*
+        we use this pre in post to generate or call the function simultaneously
+        pre -> before calling the function wt task is being performed
+        in->function call happens in between 2 function call
+        post ->  function call happens at last before terminating the call
 
          */
     }
@@ -16,5 +26,10 @@ public class PreInPost {
         System.out.print(n+" "); //in
         pip(n-1);
         System.out.print(n+" "); //post
+//        System.out.print("pre"+n+" "); //pre
+//        pip(n-1);
+//        System.out.print("in"+n+" "); //in
+//        pip(n-1);
+//        System.out.print("post"+n+" "); //post
     }
 }
