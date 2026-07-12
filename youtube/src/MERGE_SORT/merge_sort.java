6c32e1253406fc5e27ad46dfcd4a43b9204c1842
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-07-12 23:48:45 +0530

    Added and updated DSA array programs

--- youtube/src/MERGE_SORT/merge_sort.java
+++ youtube/src/MERGE_SORT/merge_sort.java
@@ -17,17 +17,20 @@ public class merge_sort {
         while(j<b.lenght) c[k++]=b[j++];
         }
          */
-        int [] arr={5,2,8,4,1,6,7,3};
+        int [] arr={5,2,8,-3,4,1,6,7,3};
         System.out.println("Array before sorting:");
         for(int elem:arr) System.out.print(elem+" ");
         merge(arr);
+        System.out.println();
         System.out.println("Array after sorting:");
         for(int elem:arr) System.out.print(elem+" ");
 
     }
     public static void merge(int [] arr){
         //step 1create 2 arrays n/2,n-n/2;
+
         int n=arr.length;
+        if(n==1) return; //one element is always sorted
         int [] a=new int [n/2];
         int [] b=new int [n-(n/2)];
         //step 2: copy paste arr into a and b
@@ -45,18 +48,19 @@ public class merge_sort {
         merge(a);
         merge(b);
         //Step 4:merge 2 sorted arrays
+        merger(a,b,arr);
 
     }
     public static void merger(int []a,int[] b,int [] c)
     {
-        int i=0,j0,k=0;
+        int i=0,j=0,k=0;
         //int [] k=new arr[a.length+b.length];
-        while(i<=a.length && j<=b.length)
+        while(i<a.length && j<b.length)
         {
-            if(a[i]<=b[j] c[k++]=a[i++];
-        else c[k++]=b[j++]
+            if(a[i]<=b[j]) c[k++]=a[i++];
+        else c[k++]=b[j++];
         }
-        while(i<a.length) c[k++]=b[i++];
-        while(j<b.lenght) c[k++]=b[j++];
+        while(i<a.length) c[k++]=a[i++];
+        while(j<b.length) c[k++]=b[j++];
     }
 }
