4d41708002b4097997fb608c2539816de8a926dc
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-07-15 23:49:44 +0530

    Added and updated DSA array programs

--- youtube/src/MERGE_SORT/InversionCount.java
+++ youtube/src/MERGE_SORT/InversionCount.java
@@ -1,8 +1,9 @@
 package MERGE_SORT;
 
 public class InversionCount {
+    static int count;
 
-    public static long inversionCount(int[] arr) {
+   /* public static long inversionCount(int[] arr) {
         return mergeSort(arr, 0, arr.length - 1);
     }
 
@@ -65,4 +66,57 @@ public class InversionCount {
 
         System.out.println("Inversion Count = " + ans);
     }
+
+    */
+   static void main(String[] args) {
+       int [] arr={4,2,9,7,8,5,3};
+       count=0;
+       for(int elem:arr)
+           System.out.print(elem+" ");
+       System.out.println();
+       merge_sort(arr);
+       for(int elem:arr)
+           System.out.print(elem+" ");
+       System.out.println("ineversion count for this array:"+count);
+
+   }
+   public static void merge_sort(int [] arr)
+   {
+       int n=arr.length;
+       if(n==1) return;
+       int [] a=new int[n/2];
+       int [] b=new int[n-(n/2)];
+       int idx=0;
+       for(int i=0;i<a.length;i++){
+           a[i]=arr[idx++];
+       }
+       for(int i=0;i<b.length;i++){
+           b[i]=arr[idx++];
+       }
+       merge_sort(a);
+       merge_sort(b);
+       merger(a,b,arr);
+   }
+   public static void merger(int [] a,int [] b,int [] arr)
+   {
+       int i=0,j=0,k=0;
+       while(i<a.length && j<b.length){
+           if(a[i]<b[j]){
+               arr[k++]=a[i++];
+           }
+           else {//if a[i]>b[j} the inversion exists for all next elements
+               count+=(a.length-i);
+           arr[k++] = b[j++];
+
+           }
+
+       }
+       while(i<a.length){
+           arr[k++]=a[i++];
+       }
+       while(j<b.length){
+           arr[k++]=b[j++];
+       }
+   }
+
 }
