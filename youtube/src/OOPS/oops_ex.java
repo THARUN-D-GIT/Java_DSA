f7d8d982bd30ee9d1fd4eed6bc968a20a2f6bd46
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-07-27 00:00:04 +0530

    Added and updated DSA array programs

--- youtube/src/OOPS/oops_ex.java
+++ youtube/src/OOPS/oops_ex.java
@@ -6,6 +6,8 @@ public class oops_ex {
         int jno;
         String name;
         int runs;
+       // final String country="India";
+        static String country="NZ";
         cricketer(int jno,String name,int runs)
         {
             this.jno=jno;
@@ -14,7 +16,7 @@ public class oops_ex {
         }
         void print()
         {
-            System.out.println(jno+" "+name+" "+runs);
+            System.out.println(jno+" "+name+" "+runs+" "+country);
         }
     }
 
@@ -23,9 +25,27 @@ public class oops_ex {
         c1.print();
         cricketer c2=new cricketer( 17,"A B Develiers",8300);
         c2.print();
+       // c2.country="england"; gives error because we can not update final keyword
         cricketer c3=c2; //Creates shallow copy means changes in c2will effect c1
         c3.runs=12500;
         c2.print();
         c3.print();
+
+        final int x=23;
+        c1.country="India"; //all players country will be India now
+       // c2.country="NZ"; // all players country will be NZ now
+        c1.print();
+        c2.print();
+         // x=26; gives error because the values can not be changes
+
+
+
+        // FINAL KEYWORD AND STATIC KEYWORD
+        /*
+        final:variables values which cannot be edited
+        static:
+        variables which can be shared between classes and methods
+         */
+
     }
 }
