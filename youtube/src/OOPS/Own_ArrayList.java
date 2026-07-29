3252c141609203ae165ddc2c5e334e4bc7370862
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-07-29 23:56:36 +0530

    Added and updated DSA array programs

---
+++ youtube/src/OOPS/Own_ArrayList.java
@@ -0,0 +1,63 @@
+package OOPS;
+
+import java.util.ArrayList;
+
+class Arraylist
+{
+    int [] arr=new int[10];
+    int idx;
+    int capacity;
+    int size;
+    Arraylist(int capacity){
+        arr=new int[capacity];
+    }
+    int capacity(){
+        return arr.length;
+    }
+    void add(int elem){
+        if(idx==arr.length){
+            int [] bigarr=new int[arr.length*2];
+            for(int i=0;i<arr.length;i++){
+                bigarr[i]=arr[i];
+            }
+            arr=bigarr; // arr is pointing to bigarr
+
+        }
+        arr[idx++]=elem;
+    }
+    void set(int index,int value)
+    {
+        arr[index]=value;
+    }
+    void display(){
+//        for(int elem:arr)
+//        {
+//            System.out.print(elem+" ");
+//        } for each loop unassigned values also
+        for(int i=0;i<idx;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    int get(int index){
+        return arr[index];
+    }
+
+}
+
+public class Own_ArrayList {
+    static void main(String[] args) {
+        Arraylist arr=new Arraylist(5);
+        System.out.println(arr.capacity());
+        arr.add(30);
+        arr.add(40);
+        arr.add(50);
+        arr.add(60);
+        arr.add(70); arr.add(80);
+        arr.display();
+        System.out.println(arr.get(3));
+        arr.set(6,98);
+        arr.display();
+
+    }
+}
