e32fd99fccec8fc6ed5dc62ca743d9a9460ae44e
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-08-15 21:54:34 +0530

    Added and updated DSA array programs

--- youtube/src/Stacks/RemoveNodesFromLL.java
+++ youtube/src/Stacks/RemoveNodesFromLL.java
@@ -19,7 +19,7 @@ public class RemoveNodesFromLL {
         }
     }
 
-    // LC method — unchanged except static
+    // METHOD 1
     static ListNode removeNodes(ListNode head) {
         Stack<ListNode> st=new Stack<>();
         ListNode temp=head;
@@ -67,6 +67,48 @@ public class RemoveNodesFromLL {
         return temp;
     }
 
+    // METHOD 2
+    static ListNode removeNodesMethod2(ListNode head) {
+
+        ListNode temp = reverse(head);
+        ListNode i = temp;
+        ListNode j = temp.next;
+
+        while (j != null) {
+
+            if (i.val > j.val)
+                j = j.next;
+
+            else {
+                i.next = j;
+                i = j;
+                j = j.next;
+            }
+        }
+
+        i.next = null;
+
+        ListNode ans = reverse(temp);
+        return ans;
+    }
+
+    static ListNode reverse(ListNode head) {
+
+        ListNode c = head;
+        ListNode p = null;
+        ListNode f = null;
+
+        while (c != null) {
+
+            f = c.next;
+            c.next = p;
+            p = c;
+            c = f;
+        }
+
+        return p;
+    }
+
     static void print(ListNode head) {
         ListNode temp = head;
 
@@ -79,6 +121,36 @@ public class RemoveNodesFromLL {
     }
 
     public static void main(String[] args) {
+        /*
+        You are given the head of a linked list.
+
+Remove every node which has a node with a greater value anywhere to the right side of it.
+
+Return the head of the modified linked list.
+
+
+
+Example 1:
+
+
+Input: head = [5,2,13,3,8]
+Output: [13,8]
+Explanation: The nodes that should be removed are 5, 2 and 3.
+- Node 13 is to the right of node 5.
+- Node 13 is to the right of node 2.
+- Node 8 is to the right of node 3.
+Example 2:
+
+Input: head = [1,1,1,1]
+Output: [1,1,1,1]
+Explanation: Every node has value 1, so no nodes are removed.
+
+
+Constraints:
+
+The number of the nodes in the given list is in the range [1, 105].
+1 <= Node.val <= 105
+         */
 
         // 5 -> 2 -> 13 -> 3 -> 8
         ListNode head = new ListNode(5);
