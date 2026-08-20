23db8f4b086d3006f47df4accfee136888a84635
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-08-20 23:27:10 +0530

    Added and updated DSA array programs

---
+++ youtube/src/Binary_Tres/PreorderTraversal.java
@@ -0,0 +1,48 @@
+package Binary_Tres;
+import java.util.*;
+ class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+public class PreorderTraversal {
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        pre(root,ans);
+        return ans;
+
+    }
+    public static void pre(TreeNode root,List<Integer> ans)
+    {
+        if(root==null) return;
+        ans.add(root.val);
+        pre(root.left,ans);
+        pre(root.right,ans);
+    }
+    static void main(String[] args) {
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(4);
+        TreeNode e = new TreeNode(5);
+        TreeNode f = new TreeNode(6);
+        TreeNode g = new TreeNode(7);
+
+        a.left = b;
+        a.right = c;
+
+        b.left = d;
+        b.right = e;
+
+        c.left = f;
+        c.right = g;
+        System.out.println(preorderTraversal(a));
+    }
+}
