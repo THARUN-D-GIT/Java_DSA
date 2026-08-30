892c8eaefe0ffb7ee19fd7a6d327907ae17dc0f0
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-08-31 00:40:09 +0530

    Added and updated DSA array programs

--- youtube/src/BinarySearchTree/DeleteNodesInBST.java
+++ youtube/src/BinarySearchTree/DeleteNodesInBST.java
@@ -3,6 +3,7 @@ package BinarySearchTree;
 public class DeleteNodesInBST {
     static void main(String[] args) {
         /*
+        LC-450
         Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
 
 Basically, the deletion can be divided into two stages:
--- youtube/src/Binary_Tres/PreorderTraversal.java
+++ youtube/src/Binary_Tres/PreorderTraversal.java
@@ -14,18 +14,19 @@ import java.util.*;
   }
 public class PreorderTraversal {
     public static List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans=new ArrayList<>();
-        pre(root,ans);
+        List<Integer> ans = new ArrayList<>();
+        pre(root, ans);
         return ans;
 
     }
-    public static void pre(TreeNode root,List<Integer> ans)
-    {
-        if(root==null) return;
+
+    public static void pre(TreeNode root, List<Integer> ans) {
+        if (root == null) return;
         ans.add(root.val);
-        pre(root.left,ans);
-        pre(root.right,ans);
+        pre(root.left, ans);
+        pre(root.right, ans);
     }
+
     static void main(String[] args) {
         TreeNode a = new TreeNode(1);
         TreeNode b = new TreeNode(2);
@@ -44,5 +45,22 @@ public class PreorderTraversal {
         c.left = f;
         c.right = g;
         System.out.println(preorderTraversal(a));
+        System.out.println("preorder traversal iterative method");
+        System.out.println(preorderTraversal_2(a));
+    }
+
+    public static List<Integer> preorderTraversal_2(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if (root == null) return ans;
+        st.push(root);
+        while (!(st.isEmpty())) {
+            TreeNode top = st.pop();
+            ans.add(top.val);
+            if (top.right != null) st.push(top.right);
+            if (top.left != null) st.push(top.left);
+        }
+        return ans;
     }
 }
