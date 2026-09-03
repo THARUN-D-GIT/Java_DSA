61bf139219224857636f33f0d673913da400ff6b
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-09-03 23:01:07 +0530

    Added and updated DSA array programs

--- youtube/src/Heap/NearlySorted.java
+++ youtube/src/Heap/NearlySorted.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class NearlySorted {
     public static void nearlySorted(int[] arr, int k) {
+
         // code here
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int idx = 0;
@@ -22,6 +23,28 @@ public class NearlySorted {
 
     public static void main(String[] args) {
         /*
+        Given an array arr[], where each element is at most k positions away from its correct position in the sorted order.
+Your task is to restore the sorted order of arr[] by rearranging the elements in place.
+
+Note: Don't use any sort() method.
+
+Examples:
+
+Input: arr[] = [2, 3, 1, 4], k = 2
+Output: [1, 2, 3, 4]
+Explanation: All elements are at most k = 2 positions away from their correct positions.
+Element 1 moves from index 2 to 0
+Element 2 moves from index 0 to 1
+Element 3 moves from index 1 to 2
+Element 4 stays at index 3
+Input: arr[]= [7, 9, 14], k = 1
+Output: [7, 9, 14]
+Explanation: All elements are already stored in the sorted order.
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ k < arr.size()
+1 ≤ arr[i] ≤ 106
+
 
          */
 
