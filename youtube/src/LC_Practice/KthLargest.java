fb22d86297bfd39e23cca03989a48f7d9b47bee4
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-09-18 23:57:13 +0530

    Added and updated DSA array programs

---
+++ youtube/src/LC_Practice/KthLargest.java
@@ -0,0 +1,59 @@
+package LC_Practice;
+
+import java.util.*;
+
+public class KthLargest {
+
+    static class Solution {
+        PriorityQueue<Integer> pq;
+        int k;
+
+        public Solution(int k, int[] nums) {
+            this.k = k;
+            pq = new PriorityQueue<>();
+
+            for (int num : nums) {
+                pq.add(num);
+
+                if (pq.size() > k) {
+                    pq.poll();
+                }
+            }
+        }
+
+        public int add(int val) {
+            pq.add(val);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+
+            return pq.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int k = 3;
+        int[] nums = {4, 5, 8, 2};
+
+        Solution obj = new Solution(k, nums);
+
+        System.out.println(obj.add(3));
+        System.out.println(obj.add(5));
+        System.out.println(obj.add(10));
+        System.out.println(obj.add(9));
+        System.out.println(obj.add(4));
+
+        System.out.println();
+
+        k = 1;
+        nums = new int[]{7};
+
+        Solution obj2 = new Solution(k, nums);
+
+        System.out.println(obj2.add(3));
+        System.out.println(obj2.add(10));
+        System.out.println(obj2.add(5));
+    }
+}
