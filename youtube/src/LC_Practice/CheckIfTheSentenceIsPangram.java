4e1468fbb1fdb709a7be69a63590682f79fbdbbf
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-09-24 21:39:50 +0530

    Added and updated DSA array programs

--- youtube/src/LC_Practice/CheckIfTheSentenceIsPangram.java
+++ youtube/src/LC_Practice/CheckIfTheSentenceIsPangram.java
@@ -22,6 +22,30 @@ public class CheckIfTheSentenceIsPangram {
 
     // Main function
     public static void main(String[] args) {
+        /*
+        LC-1832
+        A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+Example 2:
+
+Input: sentence = "leetcode"
+Output: false
+
+
+Constraints:
+
+1 <= sentence.length <= 1000
+sentence consists of lowercase English letters.
+         */
 
         CheckIfTheSentenceIsPangram obj =
                 new CheckIfTheSentenceIsPangram();
