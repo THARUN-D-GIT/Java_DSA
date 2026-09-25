410d05c205fdc21ff9afe2f47402eb449755347b
THARUN-D-GIT <tarungowda9383@gmail.com>
2026-09-25 23:57:58 +0530

    Added and updated DSA array programs

--- youtube/src/LC_Practice/SmallestIndexWithDigitSumEqualToIndex.java
+++ youtube/src/LC_Practice/SmallestIndexWithDigitSumEqualToIndex.java
@@ -71,6 +71,8 @@ Constraints:
 
 1 <= nums.length <= 100
 0 <= nums[i] <= 1000
+
+the main idea is how we calculate for 2 or more digits
          */
 
         SmallestIndexWithDigitSumEqualToIndex obj =
